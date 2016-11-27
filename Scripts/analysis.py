26800504c8f13e5b0dcae6208003d7a8772a2d6e
Nick Machairas <nick@machairas.com>
2016-11-27 01:15:18 -0500

    updates & housekeeping

--- Scripts/analysis.py
+++ Scripts/analysis.py
@@ -1,104 +1,304 @@
 import numpy as np
 
-#        omega = retaining wall inclination
-#         beta = backfill of surface slope
-#        theta = angle to the vertical
-#        alpha = angle normal to wall
-#  alpha_alpha = obliquity of the earth thrust
-#        gamma = unit weight (kN/m3)
-#          phi = internal friction angle
-#            c = cohesion (kPa)
-#            g = gravitational acceleration (9.80665 m/s2)
-#           kh = horizontal seismic coefficient
-#           kv = vertical seismic coefficient
-#      K_alpha = active lateral earth pressure coefficient
-#      alpha_h = horizontal pseudo-static acceleration
-#      alpha_v = vertical pseudo-static acceleration
-#      g_theta = modified gravitational acceleration
-#  gamma_theta = modified unit weight
-#            z = depth below sloping ground surface
-#      z_theta = modified axis along theta
-#  sigma_theta = modified effective stress
-#  sigma_alpha = stress acting on the wall
-#    sigma_AEH =
-#      J_alpha = active condition
+class sep (object):
+    """
+    Add a description here...
+    """
+
+    def __init__(self, kh, kv, omega, beta, phi, gamma, c, H, zw, g=9.80665):
+        self.kh = kh
+        self.kv = kv
+        self.g = g     # Gravitational acceleration (m/s2)
+        self.omega = np.radians(omega)
+        self.beta = np.radians(beta)
+        self.phi = np.radians(phi)
+        self.gamma = gamma    # unit weight (kN/m3)
+        self.c = c     # cohesion (kPa)
+        self.H = H     # wall height (m)
+        self.zw = zw
+
+    def alpha_h(self):
+        """
+        Horizontal component of pseudo-static acceleration
+        """
+        return self.kh * self.g
+
+    def alpha_v(self):
+        """
+        Vertical component of pseudo-static acceleration
+        """
+        return self.kv * self.g
+
+    def zl(self):
+        """
+        Depth along length of retaining wall
+        """
+        return self.zw/np.cos(self.omega)
+
+    def z(self):
+        """
+        Depth below the sloping ground surface
+        """
+        return self.zw * (np.cos(self.beta-self.omega))/(np.cos(self.beta) \
+                * np.cos(self.omega))
+
+    def g_theta(self):
+        """
+        Equation 1: Modified gravitational acceleration
+        """
+        return np.sqrt((self.g + self.alpha_v())**2 + self.alpha_h()**2)
+
+    def theta(self):
+        """
+        Equation 2: Angle to vertical
+        """
+        return np.arctan(self.kh/(1 + self.kv))
+
+    def gamma_theta(self):
+        """
+        Equation 3: Modified unit weight
+        """
+        return (self.gamma * (1 + self.kv))/(np.cos(self.theta()))
+
+    def Ja(self):
+        """
+        Equation 13: Active condition
+        """
+        J_p1 = ((self.gamma*self.z()*np.cos(self.beta)*np.cos(self.beta \
+                + self.theta())*(1+self.kv))/np.cos(self.theta())) \
+                + self.c*np.cos(self.phi)*np.sin(self.phi)
+        J_p2 = (self.gamma**2)*(self.z()**2)*((np.cos(self.beta))**2) \
+                * ((1+self.kv)**2) * ((((np.cos(self.beta+self.theta()))**2) \
+                - ((np.cos(self.phi))**2))/((np.cos(self.theta()))**2))
+        J_p3 = (self.c**2)*((np.cos(self.phi))**2)
+        J_p4 = (2*self.c*self.gamma*self.z()*np.cos(self.phi)*np.sin(self.phi) \
+                * np.cos(self.beta) * np.cos(self.beta+self.theta())*(1+self.kv)) \
+                / (np.cos(self.theta()))
+
+        return (1/((np.cos(self.phi))**2))*(J_p1 - np.sqrt(J_p2+J_p3+J_p4))
+
+    def alpha_a(self, degrees=False):
+        """
+        Equation 18: Obliquity
+        """
+        alpha_a_p1 = (((2*np.cos(self.theta())*np.cos(self.beta+self.theta())) \
+                        / (np.cos(self.beta)*(1+self.kv)))*(self.Ja() \
+                        / (self.gamma*self.z()))-1)
+        alpha_a_p2 = 2*(np.sin(self.beta-self.omega))*(np.cos(self.beta-self.omega))
+        alpha_a_p3 = 2*(np.sin(self.theta()+self.omega))*(np.cos(self.theta()+self.omega))
+
+        alpha_a = np.arctan(((alpha_a_p1*alpha_a_p2)+alpha_a_p3) \
+                    / (2*(alpha_a_p1*((np.cos(self.beta-self.omega))**2) \
+                    + ((np.sin(self.theta()+self.omega))**2))))
+
+        if degrees:
+            return np.rad2deg(alpha_a)
+        else:
+            return alpha_a
+
+    def Ka(self):
+        """
+        Equation 16: Active lateral earth pressure coefficient
+        """
+        return ((np.cos(self.beta)*(1+self.kv)*(np.sin(self.theta()+self.omega)**2 \
+                - np.cos(self.beta-self.omega)**2)) \
+               /(np.cos(self.alpha_a())*np.cos(self.beta+self.theta()) \
+               * np.cos(self.theta()))
+               ) + (
+               ((2*(self.Ja()/(self.gamma*self.z()))*np.cos(self.beta-self.omega)**2) \
+               / np.cos(self.alpha_a())))
+
+    def sigma_a(self):
+        """
+        Equation 15: stress acting on the wall
+        """
+        return self.gamma * self.z() * self.Ka()
+
+    def sigma_AEH(self):
+        """
+        Equation 19:
+        """
+        return self.sigma_a() * np.cos(self.alpha_a() + self.omega)
+
+    def Hz(self):
+        """
+        Equation 21: Vertical distance between the heel of the retaining wall
+                     and its backfill slope surface
+        """
+        return self.H * (np.cos(self.beta-self.omega)) \
+                / (np.cos(self.beta)*np.cos(self.omega))
+
+    def Hl(self):
+        """
+        Equation 23: Length of retaining wall
+        """
+        return self.H/np.cos(self.omega)
 
 
-def figure5(kh, kv, omega_deg, beta_deg, phi_deg, alpha_deg, c, gamma, H, z_w):
-    ### Gravitational & pseudo-static accelerations
-    g = 9.80665
-    alpha_h = kh * g
-    alpha_v = kv * g
+class sep2 (object):
+    """
+    Add a description here...
+    """
 
-    ### Convert degrees to radians
-    omega = np.radians(omega_deg)
-    beta = np.radians(beta_deg)
-    phi = np.radians(phi_deg)
-    alpha = np.radians(alpha_deg)
+    def __init__(self, kh, kv, omega, beta, phi, gamma, c, H, zw, g=9.80665):
+        self.kh = kh
+        self.kv = kv
+        self.g = g     # Gravitational acceleration (m/s2)
+        self.omega = np.radians(omega)
+        self.beta = np.radians(beta)
+        self.phi = np.radians(phi)
+        self.gamma = gamma    # unit weight (kN/m3)
+        self.c = c     # cohesion (kPa)
+        self.H = H     # wall height (m)
+        self.zw = zw
 
-    ### Heights
-    # z_w = H ???
-    z_l = z_w/np.cos(omega)
-    z = z_w * (np.cos(beta-omega))/(np.cos(beta)*np.cos(omega))
+    def alpha_h(self):
+        """
+        Horizontal component of pseudo-static acceleration
+        """
+        return self.kh * self.g
 
-    ### Equation 1
-    g_theta = np.sqrt((g + alpha_v)**2 + alpha_h**2)
+    def alpha_v(self):
+        """
+        Vertical component of pseudo-static acceleration
+        """
+        return self.kv * self.g
 
-    ### Equation 2
-    # theta = np.arctan(alpha_h / (g + alpha_v))
-    # or
-    theta = np.deg2rad(np.arctan(kh/(1 + kv)))
+    def zl(self):
+        """
+        Depth along length of retaining wall
+        """
+        return self.zw/np.cos(self.omega)
 
-    ### Equation 3
-    gamma_theta = (gamma * (1 + kv))/(np.cos(theta))
+    def z(self):
+        """
+        Depth below the sloping ground surface
+        """
+        return self.zw * (np.cos(self.beta-self.omega))/(np.cos(self.beta) \
+                * np.cos(self.omega))
 
-    ### Equation 4
-    sigma_theta = gamma * z * (1 + kv) * (np.cos(beta)/np.cos(theta))
+    def g_theta(self):
+        """
+        Equation 1: Modified gravitational acceleration
+        """
+        return np.sqrt((self.g + self.alpha_v())**2 + self.alpha_h()**2)
 
-    ### Equation 13
-    J_alpha = (1/(np.cos(phi)**2)) * (
-                (
-                 ((gamma*z*np.cos(beta)*np.cos(beta+theta)*(1+kv))/np.cos(theta))
-                 + c * np.cos(phi) * np.sin(phi)
-                )
-                - np.sqrt(
-                 (gamma**2 * z**2 * np.cos(beta)**2 * (1+kv)**2 *
-                    ((np.cos(beta+theta)**2 - np.cos(phi)**2)/np.cos(theta)**2))
-                 + c**2 + np.cos(phi)**2
-                 + ((2*c*gamma*z*np.cos(phi)*np.sin(phi)*np.cos(beta)*np.cos(beta+theta)*(1+kv))/np.cos(theta))
-                )
-    )
+    def theta(self):
+        """
+        Equation 2: Angle to vertical
+        """
+        return np.arctan(self.kh/(1 + self.kv))
 
-    ### Equation 16
-    K_alpha =   (
-                (np.cos(beta)*(1+kv)*(np.sin(theta+omega)**2 - np.cos(beta-omega)**2))/(np.cos(alpha)*np.cos(beta+theta)*np.cos(theta))
-                ) + (
-                ((2*(J_alpha/(gamma*z))*np.cos(beta-omega)**2)/np.cos(alpha))
-    )
+    def gamma_theta(self):
+        """
+        Equation 3: Modified unit weight
+        """
+        return (self.gamma * (1 + self.kv))/(np.cos(self.theta()))
 
-    ### Equation 15
-    sigma_alpha = gamma * z * K_alpha
+    def Ja(self, z=None):
+        """
+        Equation 13: Active condition
+        """
+        if z:
+            z = z
+        else:
+            z = self.z()
 
-    ### Equation 18
-    alpha_alpha = np.arctan(
-                (
-                 ((2*np.cos(theta)*np.cos(beta+theta)*J_alpha)/(np.cos(beta)*(1+kv)*gamma*z)-1)
-                 * np.sin(beta-omega)**2 + np.sin(theta+omega)**2
-                )/2*(
-                 ((2*np.cos(theta)*np.cos(beta+theta)*J_alpha)/(np.cos(beta)*(1+kv)*gamma*z)-1)
-                 * np.cos(beta-omega)**2 + np.sin(theta+omega)**2
-                )
-    )
+        J_p1 = ((self.gamma*z*np.cos(self.beta)*np.cos(self.beta \
+                + self.theta())*(1+self.kv))/np.cos(self.theta())) \
+                + self.c*np.cos(self.phi)*np.sin(self.phi)
+        J_p2 = (self.gamma**2)*(z**2)*((np.cos(self.beta))**2) \
+                * ((1+self.kv)**2) * ((((np.cos(self.beta+self.theta()))**2) \
+                - ((np.cos(self.phi))**2))/((np.cos(self.theta()))**2))
+        J_p3 = (self.c**2)*((np.cos(self.phi))**2)
+        J_p4 = (2*self.c*self.gamma*z*np.cos(self.phi)*np.sin(self.phi) \
+                * np.cos(self.beta) * np.cos(self.beta+self.theta())*(1+self.kv)) \
+                / (np.cos(self.theta()))
 
-    alpha_alpha_deg = np.rad2deg(alpha_alpha)
+        return (1/((np.cos(self.phi))**2))*(J_p1 - np.sqrt(J_p2+J_p3+J_p4))
 
-    ### Equation 19
-    sigma_AEH = sigma_alpha * np.cos(alpha_alpha + omega)
+    def alpha_a(self, z=None, degrees=False):
+        """
+        Equation 18: Obliquity
+        """
+        if z:
+            z = z
+        else:
+            z = self.z()
 
-    ### Results
-    #return z_w, z_l, z, J_alpha, alpha_alpha, K_alpha, sigma_alpha, sigma_AEH
-    print (round(z_w,4),round(z_l,4),round(z,4),round(J_alpha,2),round(alpha_alpha_deg,2),\
-        round(K_alpha,2),round(sigma_alpha,2),round(sigma_AEH,2))
+        alpha_a_p1 = (((2*np.cos(self.theta())*np.cos(self.beta+self.theta())) \
+                        / (np.cos(self.beta)*(1+self.kv)))*(self.Ja() \
+                        / (self.gamma*z))-1)
+        alpha_a_p2 = 2*(np.sin(self.beta-self.omega))*(np.cos(self.beta-self.omega))
+        alpha_a_p3 = 2*(np.sin(self.theta()+self.omega))*(np.cos(self.theta()+self.omega))
+
+        alpha_a = np.arctan(((alpha_a_p1*alpha_a_p2)+alpha_a_p3) \
+                    / (2*(alpha_a_p1*((np.cos(self.beta-self.omega))**2) \
+                    + ((np.sin(self.theta()+self.omega))**2))))
+
+        if degrees:
+            return np.rad2deg(alpha_a)
+        else:
+            return alpha_a
+
+    def Ka(self, z=None):
+        """
+        Equation 16: Active lateral earth pressure coefficient
+        """
+        if z:
+            z = z
+        else:
+            z = self.z()
+
+        return ((np.cos(self.beta)*(1+self.kv)*(np.sin(self.theta()+self.omega)**2 \
+                - np.cos(self.beta-self.omega)**2)) \
+               /(np.cos(self.alpha_a())*np.cos(self.beta+self.theta()) \
+               * np.cos(self.theta()))
+               ) + (
+               ((2*(self.Ja()/(self.gamma*z))*np.cos(self.beta-self.omega)**2) \
+               / np.cos(self.alpha_a())))
+
+    def sigma_a(self, z=None):
+        """
+        Equation 15: stress acting on the wall
+        """
+        if z:
+            z = z
+        else:
+            z = self.z()
+
+        return self.gamma * z * self.Ka()
+
+    def sigma_AEH(self):
+        """
+        Equation 19:
+        """
+        return self.sigma_a() * np.cos(self.alpha_a() + self.omega)
+
+    def Hz(self):
+        """
+        Equation 21: Vertical distance between the heel of the retaining wall
+                     and its backfill slope surface
+        """
+        return self.H * (np.cos(self.beta-self.omega)) \
+                / (np.cos(self.beta)*np.cos(self.omega))
+
+    def Hl(self):
+        """
+        Equation 23: Length of retaining wall
+        """
+        return self.H/np.cos(self.omega)
+
+    def zc(self):
+        """
+        Equation 20: Depth of tension crack
+        """
+        #var1 = self.Hl()
+        #var2 = self.Ja(z=var1)
+
+        #return self.Hl() * (1 - ()/())
+
+
 
 ### Test with:
-### figure5(0.2, 0.1, 20, 15, 30, 5, 20, 23, 15, 0.0001)
+### test = sep(0.2, -0.1, 20, 15, 30, 23, 20, 15, 6)
