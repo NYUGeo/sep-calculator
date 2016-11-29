9b70e154e7d9f5204093d34d9bdf067dc40ab0e8
Nick Machairas <nick@machairas.com>
2016-11-28 20:15:31 -0500

    major updates

--- scripts/web-app.py
+++ scripts/web-app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from analysis import sep
 
+from bokeh import models
 from bokeh.plotting import figure
 from bokeh.io import output_file, show, curdoc
 from bokeh.charts import HeatMap, bins, output_file, show
@@ -59,9 +60,11 @@ x_sigma.extend([0,0])
 y_sigma.extend([example.Hl(),0])
 source_example = ColumnDataSource(data=dict(x=x_sigma, y=y_sigma))
 
+TOOLS = 'pan,box_zoom,crosshair,reset,save'
 plot_sigma = figure(x_axis_label='sigma_AEH (kPa)', y_axis_label= \
-                    "Depth Along Wall Length 'zl' (m)", y_range=(example.Hl(),0), \
-                    plot_width=350, plot_height=350)
+                    "Depth Along Wall Length 'Zl' (m)", y_range=(0.99*example.Hl(),example.Hl()-30), \
+                    plot_width=250, plot_height=450, \
+                    toolbar_location="above", toolbar_sticky=False, tools=TOOLS)
 sigma_plot = Patch(x='x', y='y', fill_color = '#EEEEEE', line_color = 'black')
 plot_sigma.add_glyph(source_example, sigma_plot)
 
@@ -71,18 +74,19 @@ source_wall = ColumnDataSource(data=dict(x=x_wall,y=y_wall))
 source_earth = ColumnDataSource(data=dict(x=x_earth,y=y_earth))
 
 # Add patches to figure
-plot = figure(x_axis_label='x-axis', y_axis_label='Height (m)', y_range=(0,23), \
-                plot_width=350, plot_height=350)
+plot = figure(x_axis_label='x-axis', y_axis_label='Height (m)',  \
+                plot_width=350, plot_height=450, y_range=(0,30), \
+                toolbar_location="above", toolbar_sticky=False, tools=TOOLS)
 plot.xaxis.visible = False
 wall = Patch(x='x', y='y', fill_color = '#DCDDDE', line_color = 'black')
 plot.add_glyph(source_wall, wall)
 earth = Patch(x='x', y='y', fill_color = '#FFFF99', line_color = 'black')
 plot.add_glyph(source_earth, earth)
 
-zwi=[0.0001,(example.H/5),2*(example.H/5),3*(example.H/5),4*(example.H/5),(example.H)]
 ### Tabulating data
+zwi=[0.0001,(example.H/5),2*(example.H/5),3*(example.H/5),4*(example.H/5),(example.H)]
 source_table = ColumnDataSource(data=dict(
-        zw=[0.0001,(example.H/5),2*(example.H/5),3*(example.H/5),4*(example.H/5),(example.H)],
+        zw=zwi,
         zl=[example.zl(i) for i in zwi],
         z=[example.z(i) for i in zwi],
         Ja=[example.Ja(i) for i in zwi],
@@ -93,17 +97,17 @@ source_table = ColumnDataSource(data=dict(
 ))
 
 columns = [
-        TableColumn(field='zw',title='Zw (m)'),
-        TableColumn(field='zl',title='Zl (m)'),
-        TableColumn(field='z',title='Z (m)'),
-        TableColumn(field='Ja',title='Ja'),
-        TableColumn(field='a_a',title='a (deg)'),
-        TableColumn(field='Ka',title='Ka'),
-        TableColumn(field='s_a',title='Sa (kPa)'),
-        TableColumn(field='s_AEH',title='S_AEH (kPa)')
+        TableColumn(field='zw',title='Z_w (m)',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='zl',title='Z_l (m)',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='z',title='Z (m)',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='Ja',title='Ja',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='a_a',title='a_a (deg)',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='Ka',title='Ka',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='s_a',title='S_a (kPa)',formatter=models.NumberFormatter(format='0.000')),
+        TableColumn(field='s_AEH',title='S_AEH (kPa)',formatter=models.NumberFormatter(format='0.000'))
 ]
 
-data_table = DataTable(source=source_table, columns=columns, width=700)
+data_table = DataTable(source=source_table, columns=columns, width=850)
 
 # Callback function that updates the plot
 def update_plot(attr, old, new):
@@ -113,6 +117,8 @@ def update_plot(attr, old, new):
     H = H_value.value
     c = c_slider.value
     gamma = gamma_slider.value
+    kh = kh_slider.value
+    kv = kv_slider.value
 
     ### New Retaining Wall Coordinates
     new_xA = 0
@@ -154,15 +160,33 @@ def update_plot(attr, old, new):
     source_wall.data = dict(x=new_x_wall, y=new_y_wall)
     source_earth.data = dict(x=new_x_earth, y=new_y_earth)
     source_example.data = dict(x=new_x_sigma, y=new_y_sigma)
+    plot_sigma.y_range.start=0.99*new_example.Hl()
+    plot_sigma.y_range.end=(new_example.Hl()-30)
+
+    ### Update tabulated data
+    new_zwi=[0.0001,(new_example.H/5),2*(new_example.H/5),3*(new_example.H/5), \
+             4*(new_example.H/5),(new_example.H)]
+    source_table.data = dict(
+            zw=new_zwi,
+            zl=[new_example.zl(i) for i in new_zwi],
+            z=[new_example.z(i) for i in new_zwi],
+            Ja=[new_example.Ja(i) for i in new_zwi],
+            a_a=[new_example.alpha_a(i, degrees=True) for i in new_zwi],
+            Ka=[new_example.Ka(i) for i in new_zwi],
+            s_a=[new_example.sigma_a(i) for i in new_zwi],
+            s_AEH=[new_example.sigma_AEH(i) for i in new_zwi]
+    )
 
 
-### Sliders & Inputs
+### Sliders
 H_value = Slider(start=10,end=25,step=1,value=15,title='Retaining Wall Height (m)')
 omega_slider = Slider(start=0,end=25,step=1,value=20,title='Omega Angle (degrees)')
 beta_slider = Slider(start=0,end=20,step=1,value=15,title='Beta Angle (degrees)')
 phi_slider = Slider(start=0,end=45,step=1,value=30,title='Phi Angle (degrees)')
 c_slider = Slider(start=0,end=100,step=5,value=20,title='Cohesion (kPa)')
 gamma_slider = Slider(start=20,end=30,step=1,value=23,title='Unit Weight (kN/m3)')
+kh_slider = Slider(start=-0.3,end=0.3,step=0.1,value=0.2,title='Horizontal Seismic Coefficient')
+kv_slider = Slider(start=-0.3,end=0.3,step=0.1,value=-0.1,title='Vertical Seismic Coefficient')
 
 # Attach the callback to the 'value' property of slider
 H_value.on_change('value', update_plot)
@@ -171,19 +195,19 @@ beta_slider.on_change('value', update_plot)
 phi_slider.on_change('value', update_plot)
 c_slider.on_change('value', update_plot)
 gamma_slider.on_change('value', update_plot)
+kh_slider.on_change('value', update_plot)
+kv_slider.on_change('value', update_plot)
 
 # Make a row layout of widgetbox(slider) and plot and add it to the current document
-layout = column(row(widgetbox(H_value,omega_slider,beta_slider,phi_slider,c_slider,gamma_slider),\
-        plot, plot_sigma),data_table)
+layout = column(row(widgetbox(H_value,omega_slider,beta_slider,phi_slider, \
+                c_slider,gamma_slider,kh_slider,kv_slider),\
+                plot, plot_sigma),data_table)
 
 curdoc().add_root(layout)
 
 
 # run with:
 # bokeh serve --show web-app.py
-
-
-
 
 # Specify the name of the output file and show the result
 #output_file('web-plot.html')
